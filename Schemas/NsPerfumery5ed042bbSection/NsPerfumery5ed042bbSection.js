22fe2b00a458d07d7cd64bf0041f23e1745e96cd
Arslan <aragon.run@gmail.com>
2022-07-19 16:35:42 +0300

    Add comment

--- Schemas/NsPerfumery5ed042bbSection/NsPerfumery5ed042bbSection.js
+++ Schemas/NsPerfumery5ed042bbSection/NsPerfumery5ed042bbSection.js
@@ -1,39 +1,41 @@
-define("NsPerfumery5ed042bbSection", ["ServiceHelper"], function (ServiceHelper) {
-    return {
-        entitySchemaName: "NsPerfumery",
-        details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
-        diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
-        methods: {
-            /**
-             * Добавление действия
-             */
-            getSectionActions: function () {
-                var actionMenuItems = this.callParent(arguments);
-                actionMenuItems.addItem(
-                    this.getButtonMenuItem({
-                        Caption: { bindTo: "Resources.Strings.GetServiceInfoButtonCaption" },
-                        Click: { bindTo: "onGetServiceInfoClick" },
-                        Enabled: true,
-                    })
-                );
-                return actionMenuItems;
-            },
+define('NsPerfumery5ed042bbSection', ['ServiceHelper'], function (ServiceHelper) {
+	return {
+		entitySchemaName: 'NsPerfumery',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {
+			/**
+			 * Добавление действия
+			 */
+			getSectionActions: function () {
+				var actionMenuItems = this.callParent(arguments)
+				actionMenuItems.addItem(
+					this.getButtonMenuItem({
+						Caption: { bindTo: 'Resources.Strings.GetServiceInfoButtonCaption' },
+						Click: { bindTo: 'onGetServiceInfoClick' },
+						Enabled: true,
+					})
+				)
+				return actionMenuItems
+			},
 
-            /**
-             * Вызов веб-сервиса
-             */
-            onGetServiceInfoClick: function () {
-                ServiceHelper.callService(
-                    "NsPerfumeryService",
-                    "GetFreePerfumeCount",
-                    function (response) {
-                        var result = response.GetFreePerfumeCountResult;
-                        this.showInformationDialog(`Количество продуктов без цены: ${result}`);
-                    },
-                    {},
-                    this
-                );
-            },
-        },
-    };
-});
+			//комментарий
+
+			/**
+			 * Вызов веб-сервиса
+			 */
+			onGetServiceInfoClick: function () {
+				ServiceHelper.callService(
+					'NsPerfumeryService',
+					'GetFreePerfumeCount',
+					function (response) {
+						var result = response.GetFreePerfumeCountResult
+						this.showInformationDialog(`Количество продуктов без цены: ${result}`)
+					},
+					{},
+					this
+				)
+			},
+		},
+	}
+})
